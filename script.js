b3993c2ad52a457fbdf118ae670774372c127dcf
Kama <kamasimbeye89@gmail.com>
2024-02-10 13:59:01 +0200

    convert UserInput to to a number by using parseIn() Make result function 'return something' unlike before.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let userInput = prompt ("Pick a number: 0 for Rock, 1 is Paper, 2 is Scisors",'' );
+let userInput = parseInt(prompt ("Pick a number: 0 for Rock, 1 is Paper, 2 is Scisors",'' ));
 
 let comChoice = Math.floor(Math.random()*3);
 
@@ -20,7 +20,7 @@ function winner (userInput, comChoice ){
 }
 
 function result (){
-    winner();
+   return winner(userInput,comChoice);
 }
 
 console.log (result());

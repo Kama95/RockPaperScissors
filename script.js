f0e7a5108cffb290ea4afa29f21a0e034a21ad8a
Kama <kamasimbeye89@gmail.com>
2024-02-10 14:40:58 +0200

    Add some comments

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-//let userInput = parseInt(prompt ("Pick a number: 0 for Rock, 1 is Paper, 2 is Scisors",'' ));
+//0 is Rock
+//1 is Paper
+//2 is Scissors
 
-//let comChoice = Math.floor(Math.random()*3)
+let userInput = parseInt(prompt ("Pick a number: 0 for Rock, 1 is Paper, 2 is Scisors",'' ));
 
-//function winner (userInput, comChoice ){
-  //  if ( userInput === comChoice){
-    //    return "draw";
-    //}
-    //else if (( userInput === 0 && comChoice === 1 )|| 
-    //(userInput === 1 && comChoice === 2) ||
-    //(userInput=== 2 && comChoice ===0))
+let comChoice = Math.floor(Math.random()*3);
+
+function winner (userInput, comChoice ){
+    if ( userInput === comChoice){
+        return "draw";
+    }
+    else if (( userInput === 0 && comChoice === 1 )|| 
+    (userInput === 1 && comChoice === 2) ||
+    (userInput=== 2 && comChoice ===0))
     
-   // {
-    //    return "You lose";
-    //}
+    {
+        return "You lose";
+    }
 
-    //else {return "You Win"};
-//}
+    else {return "You Win"};
+}
 
-//function result (){
-  // return winner(userInput,comChoice);
-//}
+function result (){
+   return winner(userInput,comChoice);
+}
 
-//console.log (result());
+console.log (result());

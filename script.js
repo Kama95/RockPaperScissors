ac3707c8d5123cbc480a5283c75e1a303420dbf8
Kama <kamasimbeye89@gmail.com>
2024-02-10 13:52:07 +0200

    Create function to compare userInput and Computer and display winner

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 let userInput = prompt ("Pick a number: 0 for Rock, 1 is Paper, 2 is Scisors",'' );
 
 let comChoice = Math.floor(Math.random()*3);
-console.log (comChoice);
+
+function winner (userInput, comChoice ){
+    if ( userInput === comChoice){
+        return "draw";
+    }
+    else if ( userInput === 0 && comChoice === 1 ){
+        return "You lose";
+    }
+    else if (userInput === 1 && comChoice === 2){
+        return "You lose";
+    }
+    else if (userInput=== 2 && comChoice ===0){
+        return "You lose";
+    }
+
+    else {return "You Win"};
+}
+
+function result (){
+    winner();
+}
+
+console.log (result());

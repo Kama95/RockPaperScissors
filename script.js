356dbc9ed062a1031c8e5884b2f43ada868e04f4
Kama <kamasimbeye89@gmail.com>
2024-02-19 14:50:58 +0200

    Create function to compare paper button click to computer's choice

--- script.js
+++ script.js
@@ -28,11 +28,28 @@ function rockChoice (comOptions) {
     else {
         return "Wrong Choice";
     }
-    
-    
-
 }
 
+function paperChoice(comOptions){
+    const comChoice = Math.floor(Math.random()*comOptions.length);
+
+    if (comOptions[comChoice] === 'Rock'){
+        let result = 'You win';
+       return result;
+   }
+   else if (comOptions[comChoice] === 'Paper'){
+       let result = 'Paper'
+       return result;
+   }
+   else if (comOptions[comChoice] === 'Scissors'){
+       let result = 'You Lose';
+       return result;
+   }
+   else {
+       return "Wrong Choice";
+    
+   }
+}
 
 const rock = document.querySelector('.rock');
 const paper= document.querySelector('.paper');
@@ -41,3 +58,7 @@ const scissors = document.querySelector('.scissors');
 rock.addEventListener('click', () => {
     console.log(rockChoice(comOptions));
 });
+
+paper.addEventListener('click',() => {
+    console.log(paperChoice(comOptions));
+})

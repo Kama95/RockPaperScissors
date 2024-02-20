cc14c740e20538e268c97ff02d1a1ce36286aa37
Kama <kamasimbeye89@gmail.com>
2024-02-20 11:12:54 +0200

    create function to play a round. compare player's selection to Computer's random selection.

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ let comScore = 0;
 function computerPlay(){
     const options = ['Rock','Paper','Scissors'];
     let RandomChoice = Math.floor(Math.random()*options.length);
-    return options(RandomChoice);
+    return options[RandomChoice];
 }
+
+function playRound (playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+    return "its a draw";}
+
+
+else if (
+    (playerSelection === 'Rock' && computerSelection === 'Scissors')||
+    (playerSelection ==='Paper' && computerSelection==='Rock')||
+    (playerSelection==='Scissors'&& computerSelection==='Paper')
+){
+
+    playerScore++;
+    return 'You win this round';
+}
+
+else {
+    comScore++;
+    return 'COmputer wins this round';
+}
+}
+

50a9ab948a10d106023baa61cafed6c549ba1894
Kama <kamasimbeye89@gmail.com>
2024-02-20 10:39:44 +0200

    create function for a computer to pick a random choice among rock,paper,scissors.

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const score = document.querySelector ('div');
 let playerScore = 0;
 let comScore = 0;
 
+function computerPlay(){
+    const options = ['Rock','Paper','Scissors'];
+    let RandomChoice = Math.floor(Math.random()*options.length);
+    return options(RandomChoice);
+}
